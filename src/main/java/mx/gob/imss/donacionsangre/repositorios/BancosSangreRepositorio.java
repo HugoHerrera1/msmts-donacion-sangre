7f95bdab1651b471146e6bb79aa10123ac380ecc
itzi <wuisi_99@hotmail.com>
2022-07-20 17:39:30 -0500

    add cache

--- src/main/java/mx/gob/imss/donacionsangre/repositorios/BancosSangreRepositorio.java
+++ src/main/java/mx/gob/imss/donacionsangre/repositorios/BancosSangreRepositorio.java
@@ -1,6 +1,8 @@
 package mx.gob.imss.donacionsangre.repositorios;
 
 import mx.gob.imss.donacionsangre.modelos.MtscBancoSangre;
+
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -8,9 +10,11 @@ import java.util.List;
 
 public interface BancosSangreRepositorio extends JpaRepository<MtscBancoSangre, Long> {
 
+	@Cacheable("certificadoDefuncionFindNameBancobyId")
     @Query(value = "select * from MTSC_BANCO_SANGRE mbs where mbs.ID_BANCO_SANGRE = ?1 and mbs.IND_ACTIVO =1", nativeQuery = true)
     MtscBancoSangre findNameBancobyId(Integer idBanco);
 
+	@Cacheable("certificadoDefuncionFindBancosSangre")
     @Query(value = "select * from MTSC_BANCO_SANGRE mbs where mbs.IND_ACTIVO =1", nativeQuery = true)
     List<MtscBancoSangre> findBancosSangre();
 }
